dfc41164b4b9e11f0edd894c5bcb1888ee91ce4f
huanghailong <huanghailong@jd.com>
2018-09-11 21:20:44 +0800

    lession1 week3

---
+++ coursera/Neural_Networks_and_Deep_Learning/week3/Planar_data_classification_with_one_hidden_layer/Planar_data_classification_with_one_hidden_layer.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*- 
+#
+# Author : hhl <dnrhhl@gmail.com>
+#
+# Time : 二 11  9 2018 19:54:23
+#
+#
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+np.random.seed(1)
+
+X, Y = load_planar_dataset()
+
+#print('X.shape = ' , X.shape)
+#print('Y.shape = ' , Y.shape)
+#plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral)
+
+clf = sklearn.linear_model.LogisticRegressionCV() 
+clf.fit(X.T,Y.T)
+
+LR_predictions = clf.predict(X.T)
+accuracy = (float)(np.dot(Y,LR_predictions)+np.dot(1-Y,1-LR_predictions))/float(Y.size)
+#print('LR accuracy = ',accuracy)
+
+def layer_sizes(X,Y):
+    n_x = X.shape[0]
+    n_h = 4
+    n_y = Y.shape[0]
+    return (n_x,n_h,n_y)
+
+#X_assess, Y_assess = layer_sizes_test_case()
+#(n_x,n_h,n_y) = layer_sizes(X_assess,Y_assess)
+#print('n_x',n_x)
+#print('n_h',n_h)
+#print('n_y',n_y)
+
+def initialize_parameters(n_x, n_h, n_y):
+    np.random.seed(2)
+    W1 = np.random.randn(n_h,n_x) * 0.01
+    b1 = np.zeros((n_h,1))
+    W2 = np.random.randn(n_y,n_h) * 0.01
+    b2 = np.zeros((n_y,1))
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    return parameters
+
+n_x, n_h, n_y = initialize_parameters_test_case()
+#parameters = initialize_parameters(n_x, n_h, n_y)
+#print("W1 = " + str(parameters["W1"]))
+#print("b1 = " + str(parameters["b1"]))
+#print("W2 = " + str(parameters["W2"]))
+#print("b2 = " + str(parameters["b2"]))
+def forward_propagation(X, parameters):
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    Z1 = np.dot(W1,X) + b1
+    A1 = sigmoid(Z1)
+    Z2 = np.dot(W2,A1) + b2
+    A2 = sigmoid(Z2)
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    return A2,cache
+
+X_assess, parameters = forward_propagation_test_case()
+
+A2, cache = forward_propagation(X_assess, parameters)
+#print(np.mean(cache['Z1']) ,np.mean(cache['A1']),np.mean(cache['Z2']),np.mean(cache['A2']))
+
+def compute_cost(A2,Y,parameters): 
+    m = Y.shape[1]
+    cross_entrpy = Y*np.log(A2) + (1-Y)*np.log(1-A2)
+    cost = -(1/m)*np.sum(cross_entrpy)
+    cost = np.squeeze(cost)
+    return cost
+
+A2, Y_assess, parameters = compute_cost_test_case()
+cost = compute_cost(A2,Y_assess,parameters)
+#print('cost',cost)
+
+def backward_propagation(parameters,cache,X,Y):
+    Z1 = cache['Z1']
+    Z2 = cache['Z2']
+    A1 = cache['A1']
+    A2 = cache['A2']
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    m = X.shape[0]
+    dZ2 = A2-Y
+    dW2 = (1/m)*np.dot(dZ2,A1.T) 
+    db2 = (1/m)*np.sum(dZ2,axis=1,keepdims = True)
+    dZ1 = (1/m)*np.dot(W2.T,dZ2)*(1-np.power(A1,2))
+    dW1 = (1/m)*np.dot(dZ1,X.T)
+    db1 = (1/m)*np.sum(dZ1,axis=1,keepdims = True)
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    return grads
+parameters, cache, X_assess, Y_assess = backward_propagation_test_case()
+
+grads = backward_propagation(parameters, cache, X_assess, Y_assess)
+print ("dW1 = "+ str(grads["dW1"]))
+print ("db1 = "+ str(grads["db1"]))
+print ("dW2 = "+ str(grads["dW2"]))
+print ("db2 = "+ str(grads["db2"]))
+
+def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
+    np.random.seed(3)
+    (n_x,n_h,n_y) = layer_sizes(X,Y)
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    for i in range(0,num_iterations):
+        A2,cache = forward_propagation(X, parameters)
+        cost = compute_cost(A2, Y, parameters)
+        grads = backward_propagation(parameters, cache, X, Y)
+        pass
+    
+
+
+
+

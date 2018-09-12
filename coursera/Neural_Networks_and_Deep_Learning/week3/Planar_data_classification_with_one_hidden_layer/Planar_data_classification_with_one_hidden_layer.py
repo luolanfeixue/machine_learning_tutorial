2ef271460b263de923dd47cb68b9e46b8a1668da
huanghailong <huanghailong@jd.com>
2018-09-12 12:21:38 +0800

    first lesson week3

--- coursera/Neural_Networks_and_Deep_Learning/week3/Planar_data_classification_with_one_hidden_layer/Planar_data_classification_with_one_hidden_layer.py
+++ coursera/Neural_Networks_and_Deep_Learning/week3/Planar_data_classification_with_one_hidden_layer/Planar_data_classification_with_one_hidden_layer.py
@@ -19,7 +19,6 @@ X, Y = load_planar_dataset()
 
 #print('X.shape = ' , X.shape)
 #print('Y.shape = ' , Y.shape)
-#plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral)
 
 clf = sklearn.linear_model.LogisticRegressionCV() 
 clf.fit(X.T,Y.T)
@@ -34,11 +33,12 @@ def layer_sizes(X,Y):
     n_y = Y.shape[0]
     return (n_x,n_h,n_y)
 
-#X_assess, Y_assess = layer_sizes_test_case()
-#(n_x,n_h,n_y) = layer_sizes(X_assess,Y_assess)
-#print('n_x',n_x)
-#print('n_h',n_h)
-#print('n_y',n_y)
+X_assess, Y_assess = layer_sizes_test_case()
+(n_x,n_h,n_y) = layer_sizes(X_assess,Y_assess)
+print('layer_sizes:')
+print('n_x',n_x)
+print('n_h',n_h)
+print('n_y',n_y)
 
 def initialize_parameters(n_x, n_h, n_y):
     np.random.seed(2)
@@ -53,18 +53,20 @@ def initialize_parameters(n_x, n_h, n_y):
     return parameters
 
 n_x, n_h, n_y = initialize_parameters_test_case()
-#parameters = initialize_parameters(n_x, n_h, n_y)
-#print("W1 = " + str(parameters["W1"]))
-#print("b1 = " + str(parameters["b1"]))
-#print("W2 = " + str(parameters["W2"]))
-#print("b2 = " + str(parameters["b2"]))
+parameters = initialize_parameters(n_x, n_h, n_y)
+print('initialize_parameters:')
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
 def forward_propagation(X, parameters):
     W1 = parameters['W1']
     b1 = parameters['b1']
     W2 = parameters['W2']
     b2 = parameters['b2']
     Z1 = np.dot(W1,X) + b1
-    A1 = sigmoid(Z1)
+    A1 = np.tanh(Z1)
     Z2 = np.dot(W2,A1) + b2
     A2 = sigmoid(Z2)
     cache = {"Z1": Z1,
@@ -74,9 +76,9 @@ def forward_propagation(X, parameters):
     return A2,cache
 
 X_assess, parameters = forward_propagation_test_case()
-
 A2, cache = forward_propagation(X_assess, parameters)
-#print(np.mean(cache['Z1']) ,np.mean(cache['A1']),np.mean(cache['Z2']),np.mean(cache['A2']))
+print('forward_propagation:')
+print(np.mean(cache['Z1']) ,np.mean(cache['A1']),np.mean(cache['Z2']),np.mean(cache['A2']))
 
 def compute_cost(A2,Y,parameters): 
     m = Y.shape[1]
@@ -87,36 +89,63 @@ def compute_cost(A2,Y,parameters):
 
 A2, Y_assess, parameters = compute_cost_test_case()
 cost = compute_cost(A2,Y_assess,parameters)
-#print('cost',cost)
+print('compute_cost:')
+print('cost',cost)
 
 def backward_propagation(parameters,cache,X,Y):
-    Z1 = cache['Z1']
-    Z2 = cache['Z2']
     A1 = cache['A1']
     A2 = cache['A2']
+
     W1 = parameters['W1']
-    b1 = parameters['b1']
     W2 = parameters['W2']
-    b2 = parameters['b2']
-    m = X.shape[0]
+
+    m = X.shape[1]
     dZ2 = A2-Y
-    dW2 = (1/m)*np.dot(dZ2,A1.T) 
-    db2 = (1/m)*np.sum(dZ2,axis=1,keepdims = True)
-    dZ1 = (1/m)*np.dot(W2.T,dZ2)*(1-np.power(A1,2))
-    dW1 = (1/m)*np.dot(dZ1,X.T)
-    db1 = (1/m)*np.sum(dZ1,axis=1,keepdims = True)
+    dW2 = (1/m) * np.dot(dZ2, A1.T) 
+    db2 = (1/m) * np.sum(dZ2, axis=1, keepdims = True)
+    dZ1 = np.dot(W2.T, dZ2)*(1-np.power(A1,2))
+    dW1 = (1/m) * np.dot(dZ1, X.T)
+    db1 = (1/m) * np.sum(dZ1, axis=1, keepdims = True)
     grads = {"dW1": dW1,
              "db1": db1,
              "dW2": dW2,
              "db2": db2}
     return grads
 parameters, cache, X_assess, Y_assess = backward_propagation_test_case()
-
 grads = backward_propagation(parameters, cache, X_assess, Y_assess)
+print('backward_propagation:')
 print ("dW1 = "+ str(grads["dW1"]))
 print ("db1 = "+ str(grads["db1"]))
 print ("dW2 = "+ str(grads["dW2"]))
 print ("db2 = "+ str(grads["db2"]))
+
+def update_parameters(parameters,grads,learning_rate = 1.2):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    dW1 = grads["dW1"]
+    db1 = grads["db1"]
+    dW2 = grads["dW2"]
+    db2 = grads["db2"]
+
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    return parameters
+parameters, grads = update_parameters_test_case()
+parameters = update_parameters(parameters, grads)
+print('update_parameters:')
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
 
 def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
     np.random.seed(3)
@@ -126,13 +155,21 @@ def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
     b1 = parameters["b1"]
     W2 = parameters["W2"]
     b2 = parameters["b2"]
+
+
     for i in range(0,num_iterations):
         A2,cache = forward_propagation(X, parameters)
         cost = compute_cost(A2, Y, parameters)
         grads = backward_propagation(parameters, cache, X, Y)
-        pass
-    
+        parameters = update_parameters(parameters, grads)
+        if print_cost and i % 1000 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    return parameters
 
-
-
-
+X_assess, Y_assess = nn_model_test_case()
+parameters = nn_model(X_assess, Y_assess, 4, num_iterations=10000, print_cost=True)
+print('nn_model:')
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
